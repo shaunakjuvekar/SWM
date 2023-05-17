6c3cdf01bc5cb19533f992fd44fac680fbfbdf02
Shaunak Juvekar <shaunakjuvekar@Shaunaks-MacBook-Air.local>
2023-05-17 19:44:36 -0400

    delete functionality but with bugs

--- vite-project/src/LocationMarkers.jsx
+++ vite-project/src/LocationMarkers.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
 import "leaflet/dist/leaflet.css";
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useMapEvents } from 'react-leaflet/hooks'
 import { Marker, Popup} from 'react-leaflet';
 import {Icon, marker} from 'leaflet';
 import Button from 'react-bootstrap/Button';
+import { v4 as uuidv4 } from 'uuid'; // Import the uuid library
 
 import redIcon from "./red_icon.png";
 import AppContext from "./AppContext";
@@ -22,23 +23,35 @@ function LocationMarker(props){
 
     const echelon = useContext(AppContext);
     
-    const initial_coordinates = { lat: 37.24, lng: -80.43}
-    const [markers, setMarkers] = useState([initial_coordinates]);
+    const initial_coordinates = { lat: 37.24231, lng: -80.43173}
+    const [markers, setMarkers] = useState([{...initial_coordinates, id: uuidv4()}]);
     const [costs, setCosts] = useState([0])
     const [labels, setLabels] = useState([''])
     const [node_numbers, setNodes] = useState([0])
     const [node_index, setIndex] = useState(0)
-
-    //console.log("Inside Location Marker-> initial markers: ",  markers);
+    const [deleteFlag, setDeleteFlag] = useState(false)
+    
+    //debugger;
+    console.log("Inside Location Marker-> initial markers: ",  markers);
     //console.log("Inside Location Marker-> costs: ",  costs);
     //const [text, setText] = useState(null)
-    
+  
     const map = useMapEvents({
     click(event) {
-        let { lat,lng } = event.latlng;  
-        lat = Math.round(lat * 100000) / 100000;
-        lng = Math.round(lng * 100000) / 100000;
-        setMarkers((prevValue) => [...prevValue, {lat, lng}])
+
+        // TO DO: Exclude Submit button too in this function and then adjust clickHandler
+        console.log(event.originalEvent.srcElement.textContent)
+        console.log(event.originalEvent.srcElement.textContent!='Delete')
+        console.log(event.originalEvent.srcElement.textContent.search(/^Submit/)!=-1)
+        if (event.originalEvent.srcElement.textContent!='Delete'){
+          if (event.originalEvent.srcElement.textContent.search(/^Submit/)==-1){
+            console.log("Inside If condition")
+            let { lat,lng } = event.latlng;  
+            lat = Math.round(lat * 100000) / 100000;
+            lng = Math.round(lng * 100000) / 100000;
+            setMarkers((prevValue) => [...prevValue, {...{lat, lng}, id: uuidv4()}])
+          }
+        }
         setIndex(node_index+1)
         setNodes((prevVal) => [...prevVal, node_index+1])
         //console.log(markers)
@@ -48,18 +61,40 @@ function LocationMarker(props){
     function clickHandler(){
     
       let final_markers = []
-      for (let i=0;i<markers.length;i++){
-        let obj = markers[i]
-        if (i!=0 && i!=markers.length-1){
-          obj['cost'] = parseInt(costs[i])
-          obj['node_label'] = labels[i]
-          obj['echelon'] = echelon.echelonKey
-          obj['index'] = node_numbers[i]
-          final_markers.push(obj)
+      console.log(labels)
+      console.log(costs)
+      console.log("markers", markers)
+      if (markers.length==1 && markers[0].lat==37.24231  && markers[0].lng==-80.43173){
+        props.onSubmit([])
+      }
+      else{
+        for (let i=0;i<markers.length;i++){
+          let obj = markers[i]
+          if (deleteFlag){
+            obj['cost'] = parseInt(costs[i+1])
+            obj['node_label'] = labels[i+1]
+            obj['echelon'] = echelon.echelonKey
+            obj['index'] = node_numbers[i+1]
+            final_markers.push(obj)
+          }
+          else {
+            if (i!=0){          // && i!=markers.length-1
+              obj['cost'] = parseInt(costs[i])
+              obj['node_label'] = labels[i]
+              obj['echelon'] = echelon.echelonKey
+              obj['index'] = node_numbers[i]
+              final_markers.push(obj)
+            }
+          } 
         }
       }
-      setMarkers([{lat: 37.24, lng: -80.43}])
+     
+
+
+
+      setMarkers([{...initial_coordinates,id: uuidv4()}])
       setCosts([0])
+      setLabels([''])
       
       echelon.changeEchelon()
       
@@ -73,13 +108,18 @@ function LocationMarker(props){
         event.preventDefault();
         const cost = event.target.elements.cost.value
         const label = event.target.elements.node_label.value
-        //console.log(event)
+        console.log(label)
         setCosts((prevValue)=>[...prevValue, cost])
         setLabels((prevVal) => [...prevVal, label])
-        
+        console.log(labels)
+        map.closePopup();
     }
 
-      
+    const deleteMarker = (markerId) => {
+      setDeleteFlag(true)
+      setMarkers(prevMarkers => prevMarkers.filter(marker => marker.id !== markerId));
+    };
+    
 
 return (
   <div >
@@ -89,16 +129,21 @@ return (
          //eventHandlers={eventHandlers}
          position={[marker.lat, marker.lng]}
          icon={markerIcon} draggable={true} 
-         key={[marker.lat, marker.lng]}
+         key={marker.id}
          //onDragEnd={(event) => handleDragEnd(event, index)}
          //eventHandlers = {handleDragEnd(event, index)}
          >
            <Popup>Coordinates: {[marker.lat, ', ', marker.lng]}
-           
+            
+            <div className="del-marker">
+                <button  className="del-btn" onClick={() => deleteMarker(marker.id)}>Delete</button>
+            </div>
+            
             <form onSubmit={formHandler}>
-              <input id='cost' placeholder='Enter cost' type='text' ></input>
-              <input id='node_label' placeholder='Enter node label' type='text' ></input>
-              <button className='form-button'>Submit</button>
+              <input id='cost' placeholder=' Enter cost' type='text' ></input>
+              <input id='node_label' placeholder=' Enter node label' type='text' ></input>
+              <div className="button-div"><button className='form-button'>Submit</button></div>
+              
             </form>
            </Popup>
          </Marker>
@@ -116,22 +161,36 @@ export default LocationMarker;
 /*
 
 
+    useEffect(() => {
+      console.log("Inside UseEffect() ", markers);
+    }, [markers]);
 
     /*
-    const deleteMarker = (event) => {
+    const deleteMarker = (markerId) => {
       console.log("Inside delete Marker")
       //console.log(event.target.parentNode.parentNode)
+      
       const val = event.target.parentNode.parentNode.textContent
+      console.log(val)
       //Coordinates: 37.22, -80.39DeleteSubmit
       let pattern1 = /:\s\d/
       const lat = parseFloat(val.substring(val.search(pattern1)+2, val.indexOf(',')));
       let pattern2 = /\dDelete/
       const long = parseFloat(val.substring(val.indexOf(',')+2,val.search(pattern2)+1));
       
-      let final_markers = markers.filter((position)=> position.lat!=lat && position.lng!=long)
-      //console.log(final_markers)
+      console.log(markers)
+      console.log(markerId)
+      //let final_markers = markers.filter((position)=> position.lat!=lat && position.lng!=long)
+      let final_markers = markers.filter((m)=> m.id!=markerId)
+      console.log(final_markers)
       setMarkers(final_markers)
     }
+    */
+
+
+
+    /*
+   
     
       ######
       Inside Popup
