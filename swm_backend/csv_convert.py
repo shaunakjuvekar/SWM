d5302d5b62616c7b53da31fa1c9cdc15fc97f7c4
Shaunak Juvekar <shaunakjuvekar@Shaunaks-MacBook-Air.local>
2023-07-02 15:21:50 -0400

    Added a LOT of UI code as well as akshat's gurobi modifications

--- swm_backend/csv_convert.py
+++ swm_backend/csv_convert.py
@@ -3,44 +3,58 @@ import ast
 import json
 import re
 
-
-#def convert_csv():
 def main():
     frontend_data = []
     print("csv_convert called from app.py")
-    with open('sample_output.csv', 'r', newline='') as inputFile, open('output_final.csv', 'w', newline='') as writerFile:
+    with open('sample_output.csv', 'r', newline='') as inputFile, \
+         open('output_final1.csv', 'w', newline='') as writerFile1, \
+         open('output_final2.csv', 'w', newline='') as writerFile2:
         
         read_file = csv.reader(inputFile)
-        write_file = csv.writer(writerFile, delimiter=';')
+        write_file_1 = csv.writer(writerFile1, delimiter=';')
+        write_file_2 = csv.writer(writerFile2, delimiter=';')
         row_number=0
+        file1_flag, file2_flag = False, False
         for row in read_file:
             obj = {}
             lat, long, id, cost_demand, node_label = row[0], row[1], row[2], row[3], row[4]
             echelon, index = row[5], row[6]
-            #facility_costs ,facility_sizes, vehicle_capacity =  row[7], row[8], row[9]
             routes, route_costs = row[10], row[11]
-            #capacity, capacity_costs = row[12], row[13]
             obj['lat'], obj['lng'], obj['label'], obj['echelon'] =lat,long, node_label, echelon
-            print(route_costs)
             obj['routes'], obj['route_costs'] = routes, route_costs
-           
+          
+            if echelon=='2' and file1_flag==False:
+                write_file_1.writerow([' -', 'Facility', ' Echelon'])
+                file1_flag = True
+            elif echelon=='3' and file2_flag==False:
+                write_file_2.writerow([' -', 'Facility', ' Echelon'])
+                file2_flag = True
+            else:
+                print("This echelon number not handled : ", echelon)
             frontend_data.append(obj)
             if routes and row_number!=0:
                 #print("routes:" , routes)
-                write_file.writerow(['Facility', ' Echelon'])
-                write_file.writerow([node_label, echelon])
-                #write_file.writerow([' ', 'Selection', 'Cost'])
-                #write_file.writerow(['Capacity', capacity, capacity_costs])
-                #write_file.writerow(['Location', node_label, cost_demand])
-                write_file.writerow(['Vehicle', 'Node Label(s) served in echelon '+str(int(echelon)-1), 'Route Cost'])
-            
+                if echelon=='2':
+                    write_file_1.writerow(['Values ', node_label, echelon])
+                    write_file_1.writerow(['Vehicle', 'Node Label(s) served in echelon '+str(int(echelon)-1), 'Route Cost'])
+                elif echelon=='3':
+                    write_file_2.writerow(['Values ', node_label, echelon])
+                    write_file_2.writerow(['Vehicle', 'Node Label(s) served in echelon '+str(int(echelon)-1), 'Route Cost'])
+                else:
+                    print("This echelon number not handled : ", echelon)
                 routes_arr = (ast.literal_eval(routes))
                 costs_arr = (ast.literal_eval(route_costs))
                 for i in range(len(routes_arr)):
                     truncated_cost = round(float(costs_arr[i]), 2)
-                    write_file.writerow([i+1, routes_arr[i], truncated_cost])
+                    if echelon=='2':
+                        write_file_1.writerow([i+1, routes_arr[i], truncated_cost])
+                    elif echelon=='3':
+                         write_file_2.writerow([i+1, routes_arr[i], truncated_cost])
+                    else:
+                        print("This echelon number not handled : ", echelon)    
             
-                write_file.writerow([])
+                write_file_1.writerow([])
+                write_file_2.writerow([])
             row_number+=1
         #print(frontend_data)
 
@@ -48,4 +62,4 @@ def main():
     with open("data_file.json", "w") as wf:
         print("Writing to data_file.json")
         json.dump(frontend_data[1:], wf)
-main()
+#main()
