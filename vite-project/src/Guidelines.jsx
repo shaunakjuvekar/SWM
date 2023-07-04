3b00a0adc7e0c3856cde93b158fe7e5da75fc1d4
Shaunak Juvekar <shaunakjuvekar@Shaunaks-MacBook-Air.local>
2023-07-04 19:35:02 -0400

    added screenshots to Guidelines and other UI changes

--- vite-project/src/Guidelines.jsx
+++ vite-project/src/Guidelines.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import "./Guidelines.css";
+import searchBar from "./assets/screenshots/Search bar.png";
+import marker from "./assets/screenshots/Filled marker.png";
+import cost_validation from "./assets/screenshots/Cost validation.png";
+import label_check from "./assets/screenshots/Label check.png";
+import text_area from "./assets/screenshots/Right text area.png";
+import route_buttons from "./assets/screenshots/Route buttons.png";
+import popups from "./assets/screenshots/Toggle popups.png";
+import routes from "./assets/screenshots/Routes.png";
+import routeTables from "./assets/screenshots/Route tables.png";
+import summaryTables from "./assets/screenshots/Summary tables.png";
+
 
 function Guidelines(){
+
+    const imgStyles1 = {
+        height: 300,
+        width: 500,
+        marginTop: 20,
+        marginBottom: 20,
+        paddingRight: 10
+    }
+
+    const imgStyles2 = {
+        height: 400,
+        width: 700,
+        marginTop: 20,
+        marginBottom: 20,
+        paddingRight: 10
+    }
 
 
 
@@ -20,6 +47,7 @@ We’ve outlined below the steps to be followed for using the system :-
                 <li>Firstly, the user can go to the <b>Map</b> link found on the navigation bar.</li>
                 <li>The map navigation is quite user friendly and intuitive, the user can scroll to zoom in/out and can navigate around by left clicking and then moving the mouse cursor accordingly.</li>
                 <li>The user can then select the designated planning area in the <b>search bar</b> located to the top left corner of the map. They can enter both the name of the area or the zip code.</li>
+                <img src={searchBar} style={imgStyles1}></img>
                 <li>Once in the area, the user can start placing markers on the map by left clicking on their desired location. The markers have the following options:- </li>
                 <ul>
                     <li><b>Demand/Location cost</b> - User can enter the appropriate value (number) here.</li>
@@ -27,7 +55,12 @@ We’ve outlined below the steps to be followed for using the system :-
                     <li><b>Delete button</b> - User can delete the marker after it has been placed.</li>
                     <li><b>Submit</b> - The values entered by the user are saved for that particular marker.</li>
                 </ul>
+                <img src={marker} style={imgStyles1}></img>
+                <li>There are a few checks implemented in the system such as user *must* assign a label to every marker in a level and the demand/facility cost *must* be a number.</li>
+                <img src={cost_validation} style={imgStyles1}></img>
+                <img src={label_check} style={imgStyles1}></img>
                 <li>Once all the markers for a particular echelon have been placed, the user can click the <b>Submit</b> button at the top right corner. This will clear out all the markers for the next level.</li>
+                
                 <li>Starting from Echelon 2, the user has the additional option of entering <b>Facility size(s), Facility cost(s) and Vehicle capacity</b>.</li>
                 <p>A sample input for these values would be :-</p>
                 <ul>
@@ -35,21 +68,24 @@ We’ve outlined below the steps to be followed for using the system :-
                     <li><b>Facility cost(s)</b> - 8, 12, 16  (comma separated values)</li>
                     <li><b>Vehicle capacity</b> - 9</li>
                 </ul>
-                    
-                <li>There are a few checks implemented in the system such as user *must* assign a label to every marker in a level and the demand/facility cost *must* be a number.</li>
+                <img src={text_area} style={imgStyles1}></img>    
                 <li>Once all the markers for all levels have been placed, the user can click the <b>Calculate</b> button. This will send all the data the user has input to the backend for processing.</li>
                 <li>After this, a <b>View Routes</b> button will be visible. Once the user clicks on it, there will be multiple buttons visible on the map. A quick description of these buttons :-</li>
+                <img src={route_buttons} style={imgStyles2}></img>    
                 <ul>
                     <li><b>Show All Facilities</b> - Shows all activated facilities based on the user input on the map.</li>
                     <li><b>Show All Routes</b> - Shows all the routes from all levels on the map.</li>
+                    <img src={routes} style={imgStyles2}></img>
                     <li><b>Echelon level Routes</b> - Dropdown which shows routes based on echelon level.</li>
                     <li><b>Facility Level Routes</b> - Dropdown which shows routes based on facility level.</li>
                     <li><b>Toggle Popups</b> - This button activates/deactivates all the popups of the nodes for better readability of the routes.</li> 
                 </ul>	
+                <img src={popups} style={imgStyles1}></img>
                 <li>For a table representation of the routes, the user can click on the <b>Route Tables</b> link on the navigation bar. 
                 After clicking on the <b>Show Tables</b> button, two tables will be displayed on the page, one table each per echelon level. In addition, user can click on the download buttons to download the respective CSV route file on their local machine.</li>
-                <li>For a summary representation, the user can click on the <b>Summary Tables</b> link on the navigation bar. After clicking on the <b>Show Tables</b> button, two tables will be displayed on the page, one echelon level and one facility level summary table. In addition, user can click on the download buttons to download the respective CSV route file on their local machine.
-                </li>
+                <img src={routeTables} style={imgStyles2}></img>    
+                <li>For a summary representation, the user can click on the <b>Summary Tables</b> link on the navigation bar. After clicking on the <b>Show Tables</b> button, two tables will be displayed on the page, one echelon level and one facility level summary table. In addition, user can click on the download buttons to download the respective CSV route file on their local machine.</li>
+                <img src={summaryTables} style={imgStyles2}></img>    
         </ol>
     </div>
     </div>
