5c51b18d0b19b70a69f323c8cb3a6a6e8e617d58
Shaunak <gamershaunj@gmail.com>
2023-12-16 18:23:11 -0500

    updated dockerfile and base url

--- vite-project/src/constants.js
+++ vite-project/src/constants.js
@@ -1,6 +1,4 @@
 export const baseURL = {
     sweep:
-      process.env.NODE_ENV === "production"
-        ? "https://sweep-backend.discovery.cs.vt.edu"
-        : "http://localhost:5002",
+        "https://sweep-backend.discovery.cs.vt.edu"
   };
